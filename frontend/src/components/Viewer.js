c6bddc72988ecba6427f818a46c13059a362fad6
m-triple-m <triplem656@gmail.com>
2022-07-26 18:45:33 +0530

    ..

--- frontend/src/components/Viewer.js
+++ frontend/src/components/Viewer.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import {Button} from '@mui/material'
+import {Button} from '@mui/material';
+import JsxParser from 'react-jsx-parser';
 const Viewer = () => {
+
+  const raw_code = `<Button variant="contained" color='error'>Nice</Button>`;
  return (
     <div className='viewer'>
         <h3 style={{color:'black'}}> <b>Component Name</b> </h3>
         <h4 style={{color:'black'}}> <b>Uploaded By</b> </h4>
-        <div className='viewerdiv'></div>
+        <div className='viewerdiv'>
+        <JsxParser jsx={raw_code} components={{ Button }} />
+        </div>
         <Button  variant="contained" type="submit" color='primary' sx={{mt:5}}>Share Code</Button>
         <div className='viewerdiv2'></div>
         
