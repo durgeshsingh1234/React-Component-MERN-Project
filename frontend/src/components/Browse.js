284ef6bae2a67513450db88888299e418e474050
durgeshsingh1234 <durgeshsingh4653@gmail.com>
2022-08-09 19:22:01 +0530

    changes

--- frontend/src/components/Browse.js
+++ frontend/src/components/Browse.js
@@ -44,6 +44,63 @@ const Browse = () => {
 
   return (
     <div className='browsebackground'>
+
+{/* header start */}
+      <div>
+      <header>
+ 
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarExample01"
+        aria-controls="navbarExample01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      {/* <div class="collapse navbar-collapse" id="navbarExample01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div> */}
+    </div>
+  </nav>
+  
+
+  
+  <div class=" browseheader p-5 text-center bg-image">
+    <div class="mask">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Components</h1>
+          <h4 class="mb-3">Showcase of various components</h4>
+          {/* <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+          >Call to action</a
+          > */}
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+</header>
+      </div>
+      {/* header end */}
+
       {/* For Searchbar */}
       <div className='browsesearchbar' >
       <div class="input-group">
